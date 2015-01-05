2329405cd9d089ae41860683d25143c8b06dbde0
dwolner <daniel.wolner@gmail.com>
2015-01-05 10:32:05 -0800

    Update single-image.php

--- portfolio-modal/single-image.php
+++ portfolio-modal/single-image.php
@@ -1,6 +1,89 @@
 <div class="figure <?php echo implode( ' ', $figure_classes ); ?> <?php echo $image_stretch  ?>" data-os-animation="<?php echo $scroll_animation; ?>" data-os-animation-delay="<?php echo $scroll_animation_delay; ?>s">
     <?php if( !empty( $hover_link ) ) : ?>
-        <a href="<?php echo $hover_link; ?>" class="figure-image <?php echo implode( ' ', $hover_link_classes ); ?>" data-links="<?php echo implode( ',', $gallery_images ); ?>" target="<?php echo $link_target; ?>">
+        <?php if( $hover_link == 'html' ): ?>
+            <div id="portfolio-html-container-<?php echo get_the_ID() ?>" class="portfolio-html-container mfp-hide mfp-with-zoom">
+                <button title="Close (Esc)" type="button" class="mfp-close">×</button>
+                <h2><?php echo get_the_title()?></h2>
+                <div><?php echo get_the_content()?></div>
+            </div>
+            <a href="#portfolio-html-container-<?php echo get_the_ID() ?>" class="open-popup-link-<?php echo get_the_ID() ?> figure-image">
+            <script type="text/javascript">
+                jQuery(function() {
+
+                        jQuery('.open-popup-link-<?php echo get_the_ID() ?>').magnificPopup({
+                            type: 'inline',
+                            removalDelay: 300,
+                            midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+                        	mainClass: 'mfp-fade'
+                        });
+
+                });
+            </script>
+            <style type="text/css">
+                .portfolio-html-container{
+                    max-width: 1200px;
+                    margin: 0 auto;
+                    background: #fff;
+                    padding: 15px;
+                    box-shadow: 1px 1px 10px #ccc;
+                    margin-top: 50px;
+                    position: relative;
+                }
+                .portfolio-html-container h2{
+                    padding-bottom: 20px;
+                }
+
+                .portfolio-html-container .mfp-close{
+                    top: 0;
+                }
+                @media (max-width: 1200px){
+                    .portfolio-html-container {
+                        max-width: 1000px;
+                    }
+                }
+
+                @media (max-width: 991px){
+                    .portfolio-html-container {
+                        max-width: 780px;
+                    }
+                }
+                /* overlay at start */
+.mfp-fade.mfp-bg {
+  opacity: 0;
+
+  -webkit-transition: all 0.15s ease-out;
+  -moz-transition: all 0.15s ease-out;
+  transition: all 0.15s ease-out;
+}
+/* overlay animate in */
+.mfp-fade.mfp-bg.mfp-ready {
+  opacity: 0.8;
+}
+/* overlay animate out */
+.mfp-fade.mfp-bg.mfp-removing {
+  opacity: 0;
+}
+
+/* content at start */
+.mfp-fade.mfp-wrap .mfp-content {
+  opacity: 0;
+
+  -webkit-transition: all 0.15s ease-out;
+  -moz-transition: all 0.15s ease-out;
+  transition: all 0.15s ease-out;
+}
+/* content animate it */
+.mfp-fade.mfp-wrap.mfp-ready .mfp-content {
+  opacity: 1;
+}
+/* content animate out */
+.mfp-fade.mfp-wrap.mfp-removing .mfp-content {
+  opacity: 0;
+}
+            </style>
+        <?php else: ?>
+            <a href="<?php echo $hover_link; ?>" class="figure-image <?php echo implode( ' ', $hover_link_classes ); ?>" data-links="<?php echo implode( ',', $gallery_images ); ?>" target="<?php echo $link_target; ?>">
+        <?php endif;?>
     <?php else : ?>
         <span class="figure-image">
     <?php endif; ?>
