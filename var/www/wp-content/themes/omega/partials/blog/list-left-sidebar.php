4dc9312dffedffc8fb0ac0b9c3341df4357655a5
dwolner <daniel.wolner@gmail.com>
2015-02-10 10:56:09 -0800

    original

---
+++ var/www/wp-content/themes/omega/partials/blog/list-left-sidebar.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row element-normal-top element-normal-bottom">
+        <div class="col-md-9">
+            <?php get_template_part( 'partials/blog/loop' ); ?>
+        </div>
+        <div class="col-md-3 sidebar">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
